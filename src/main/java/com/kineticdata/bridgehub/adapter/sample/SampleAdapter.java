57912f6e53c84e60d2fb5c2e8df8e7e0310926e4
Chad Rehm <chad.rehm@kineticdata.com>
2021-10-26 08:18:59 -0500

    Implemented BridgeAdapter interface Implemented the bridge adapter interface in the "main" class.  Also implemented all abstract methods.

--- src/main/java/com/kineticdata/bridgehub/adapter/sample/SampleAdapter.java
+++ src/main/java/com/kineticdata/bridgehub/adapter/sample/SampleAdapter.java
@@ -1,14 +1,63 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This is a sample starting adapter used by kinetic data to train bridge adapter
+ * development.
  */
 package com.kineticdata.bridgehub.adapter.sample;
 
+import com.kineticdata.bridgehub.adapter.BridgeAdapter;
+import com.kineticdata.bridgehub.adapter.BridgeError;
+import com.kineticdata.bridgehub.adapter.BridgeRequest;
+import com.kineticdata.bridgehub.adapter.Count;
+import com.kineticdata.bridgehub.adapter.Record;
+import com.kineticdata.bridgehub.adapter.RecordList;
+import com.kineticdata.commons.v1.config.ConfigurablePropertyMap;
+import java.util.Map;
+
 /**
  *
- * @author chadrehm
+ * @author kineticdata
+ * @date   10-26-2021
  */
-public class SampleAdapter {
+public class SampleAdapter implements BridgeAdapter {
+
+    @Override
+    public ConfigurablePropertyMap getProperties() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setProperties(Map<String, String> properties) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getVersion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void initialize() throws BridgeError {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public Count count(BridgeRequest request) throws BridgeError {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Record retrieve(BridgeRequest request) throws BridgeError {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public RecordList search(BridgeRequest request) throws BridgeError {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
